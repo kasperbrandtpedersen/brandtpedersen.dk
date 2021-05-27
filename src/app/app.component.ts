77f47c3d3f4cd534b32c5c04f04bdf0b63b6ddc5
kbp <kasperbrandtpedersen0@gmail.com>
2021-05-27 22:19:54 +0200

    some basic stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'brandtpedersendk';
+  title = 'Brandt Pedersen';
+  isAuthenticated = false;
+  fetching = false;
+
+  constructor(private appService: AppService) {
+    this.isAuthenticated = this.appService.isAuthenticated();
+  }
 }
