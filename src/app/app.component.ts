ec492bc09b1689db4d546ce53f5736e70dc8b07f
Kasper Brandt Pedersen <kabp@universal-robots.com>
2024-09-24 21:13:19 +0200

    sb admin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import {  TopbarComponent } from './topbar/topbar.component';
+import {  FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    TopbarComponent,
+    SidebarComponent,
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
   styles: [],
 })
---
+++ src/app/topbar/topbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-topbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './topbar.component.html',
+})
+export class TopbarComponent {
+
+}
